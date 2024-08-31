20933f3e1285b4ba58f4be49ad2f1e86620ef21a
Priyanshu Maurya <user.email=106907193+satyasaadhika@users.noreply.github.com>
2024-08-31 15:12:12 +0000

    Terminate route match for '/menu' route

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -9,7 +9,7 @@ export default function Navbar() {
       <ul className="Navbar-items">
         <li className="Navbar-item"><NavLink to='/' end>Home</NavLink></li>
         <li className="Navbar-item"><NavLink to='/about'>About</NavLink></li>
-        <li className="Navbar-item"><NavLink to='/menu'>Menu</NavLink></li>
+        <li className="Navbar-item"><NavLink to='/menu'end>Menu</NavLink></li>
         <li className="Navbar-item"><NavLink to='/booking'>Reservations</NavLink></li>
         <li className="Navbar-item"><NavLink to='/order-online'>Online Order</NavLink></li>
         <li className="Navbar-item"><NavLink to='/login'>Login</NavLink></li>
