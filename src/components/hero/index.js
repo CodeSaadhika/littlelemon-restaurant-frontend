a9717566073b1ad97e2f9a98b2d5d086a5127dea
Priyanshu Maurya <user.email=106907193+satyasaadhika@users.noreply.github.com>
2024-08-31 15:10:28 +0000

    Update hero banner for consistent grid pattern

--- src/components/hero/index.js
+++ src/components/hero/index.js
@@ -6,19 +6,23 @@ import './Herobanner.css';
 export default function Herobanner(props) {
     return(<section className="Herobanner">
         <div className="container">
-            <div className="row Herobanner-content">
-                <div className="col-5 row flex-column Herobanner-text">
-                    <div className="row flex-column">
-                        <h1>Little Lemon</h1>
-                        <h2>Chicago</h2>
-                        <p>In voluptate nulla qui ullamco ad laborum minim elit non laborum cupidatat occaecat amet. Veniam nisi nostrud non Lorem in ea. Laboris labore cupidatat incididunt ut commodo proident cillum id elit esse est.</p>
+            <div className="row">
+                <div className="col-8 m-2">
+                    <div className="row Herobanner-content">
+                        <div className="col-7 row flex-column Herobanner-text">
+                            <div className="row flex-column">
+                                <h1>Little Lemon</h1>
+                                <h2>Chicago</h2>
+                                <p>In voluptate nulla qui ullamco ad laborum minim elit non laborum cupidatat occaecat amet. Veniam nisi nostrud non Lorem in ea. Laboris labore cupidatat incididunt ut commodo proident cillum id elit esse est.</p>
+                            </div>
+                            <div className="col-6">
+                                <NavLink to='/booking' className="button button--secondary Herobanner-cta">Reserve a Table</NavLink>
+                            </div>
+                        </div>
+                        <div className="col-5 Herobanner-image">
+                            <img className="img" src="/images/sandwich.jpg" alt="Sandwich" />
+                        </div>
                     </div>
-                    <div className="col-6">
-                        <NavLink to='/booking' className="button button--regular Herobanner-cta">Reserve a Table</NavLink>
-                    </div>
-                </div>
-                <div className="col-3 Herobanner-image">
-                    <img className="img" src="/images/sandwich.jpg" alt="Sandwich" />
                 </div>
             </div>
         </div>
