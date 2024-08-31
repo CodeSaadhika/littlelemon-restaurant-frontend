c4d26331c2395702949265041d5b3e2c222540e1
Priyanshu Maurya <user.email=106907193+satyasaadhika@users.noreply.github.com>
2024-08-31 15:10:01 +0000

    Update navigation header for consistent grid pattern

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -7,10 +7,16 @@ import Navbar from "../navbar";
 export default function Header(props) {
   return (<header className="container Header">
     <div class="row" >
-      <div className="col-4 Header-logo">
-        <a href="/"><img src="/images/logo.jpg" alt="Little Lemon" /></a>
-      </div>
-      <Navbar />
+        <div className="row">
+            <div className="col-8 m-2">
+                <div className="row">
+                    <div className="col-4 Header-logo">
+                        <a href="/"><img src="/images/logo.jpg" alt="Little Lemon" /></a>
+                    </div>
+                    <Navbar />
+                </div>
+            </div>
+        </div>
     </div>
   </header >);
 }
